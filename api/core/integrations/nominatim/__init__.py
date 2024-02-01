377b3f97a7b356346ca4d997372dbbfb0dc69086
hpgy <hpougy@gmail.com>
2024-02-01 11:59:29 -0300

    returning configured object

--- api/core/integrations/nominatim/__init__.py
+++ api/core/integrations/nominatim/__init__.py
@@ -1,1 +1,6 @@
 from .nominatim import Nominatim
+from api.config import CITY, STATE, COUNTRY_ISO, NOMINATIM_EMAIL
+
+
+print(COUNTRY_ISO)
+nominatim_address_search = Nominatim(CITY, STATE, COUNTRY_ISO, NOMINATIM_EMAIL)
