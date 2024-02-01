506ab68e8433f11b66eb07db0d9222d1babb0628
hpgy <hpougy@gmail.com>
2024-02-01 11:41:57 -0300

    solving when resp obj has no text

--- api/core/utils/json_decode.py
+++ api/core/utils/json_decode.py
@@ -6,4 +6,9 @@ def json_resp_decode_error(resp:Response)->dict:
     try:
         return resp.json()
     except json.JSONDecodeError:
-        return {'erro' : resp.txt}
+        try:
+            return {'erro' : resp.txt,
+                    'tipo_erro' : 'JsonDecodeError'}
+        except AttributeError:
+            return {'erro' : str(resp.content),
+                    'tipo_erro' : 'JsonDecodeError'}
