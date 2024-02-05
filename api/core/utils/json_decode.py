34c81f08b4207b979bd00d6a353a701960c17ae8
h-pgy <hpougy@gmail.com>
2024-02-05 11:59:46 -0300

    xml response to dict parsing

--- api/core/utils/json_decode.py
+++ api/core/utils/json_decode.py
@@ -1,4 +1,5 @@
 import json
+import xmltodict
 from requests import Response
 
 def json_resp_decode_error(resp:Response)->dict:
@@ -12,3 +13,13 @@ def json_resp_decode_error(resp:Response)->dict:
         except AttributeError:
             return {'erro' : str(resp.content),
                     'tipo_erro' : 'JsonDecodeError'}
+        
+
+def xml_to_json_decode_error(resp:Response)->dict:
+
+    try:
+        t = resp.text
+        return xmltodict.parse(t)
+    except (AttributeError, xmltodict.expat.ExpatError):
+        return {'erro' : str(resp.content),
+                    'tipo_erro' : 'XMLDecodeError'}
