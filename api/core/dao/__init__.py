108207f9efc1c6b28e229ba4f4d86abad424106b
h-pgy <hpougy@gmail.com>
2024-02-05 22:59:31 -0300

    exposing dao obj as functions

---
+++ api/core/dao/__init__.py
@@ -0,0 +1,6 @@
+from .camadas_search import ListCamadas, DetailCamada
+from .address_search import AddresSearch
+
+listar_camadas = ListCamadas()
+detalhar_camada = DetailCamada()
+buscar_endereco = AddresSearch()
