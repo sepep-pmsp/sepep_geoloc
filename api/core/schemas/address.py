670e2b5de70716cc7e80c4866055030219cc8e73
h-pgy <hpougy@gmail.com>
2024-02-05 23:44:25 -0300

    endpoint como post - funciona melhor

--- api/core/schemas/address.py
+++ api/core/schemas/address.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, validator
-from typing import Dict
+from typing import List, Dict, Optional
 
 from .geojson import GeoJson
 
@@ -9,4 +9,22 @@ class AdressSearch(BaseModel):
     endereco: GeoJson
     camadas_geosampa: Dict[str, GeoJson]
 
+class CamadaParam(BaseModel):
+
+    alias: str
+    layer_name: str
+
+    @validator('layer_name')
+    def validar_layer_name(cls, value)->str:
+
+        value = str(value)
+        if not value.startswith('geoportal:'):
+            value = f'geoportal:{value}'
+        
+        return value
+
+class AdressSearchParameters(BaseModel):
+
+    endereco: str
+    camadas: Optional[List[CamadaParam]]=None
     
--- api/v1/endpoints/endereco.py
+++ api/v1/endpoints/endereco.py
@@ -1,18 +1,25 @@
 from fastapi import APIRouter, Query
 
-from typing import Dict
+from typing import List
 
 from core.dao import buscar_endereco
-from core.schemas.address import AdressSearch
+from core.schemas.address import AdressSearch, AdressSearchParameters
 
 
 
 app = APIRouter()
 
 
-@app.get('/geolocalizar_endereco/{endereco}', response_model=AdressSearch, tags=['geolocalizacao'])
-async def geolocalizar_endereco(endereco:str, **camadas_geosampa:Dict[str, str]):
+@app.post('/geolocalizar_endereco/', response_model=List[AdressSearch], tags=['geolocalizacao'])
+async def geolocalizar_endereco(search_endereco:AdressSearchParameters):
 
+    endereco = search_endereco.endereco
+    camadas = search_endereco.camadas
+
+    if camadas:
+        camadas_geosampa = {camada.alias:camada.layer_name for camada in camadas}
+    else:
+        camadas_geosampa={}
     data = buscar_endereco(endereco, **camadas_geosampa)
 
     return data
