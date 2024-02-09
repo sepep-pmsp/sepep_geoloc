9790c2351e2c576753bef683132e2ec168116342
hpgy <hpougy@gmail.com>
2024-02-09 15:10:32 -0300

    endpoint geolocalizacao sem integracao

--- api/v1/endpoints/endereco.py
+++ api/v1/endpoints/endereco.py
@@ -2,16 +2,16 @@ from fastapi import APIRouter, Query
 
 from typing import List
 
-from core.dao import buscar_endereco
-from core.schemas.address import AdressSearch, AdressSearchParameters
+from core.dao import buscar_endereco, buscar_endereco_simples
+from core.schemas.address import AdressSearch, AdressSearchParameters, GeoJsonEndereco
 
 
 
 app = APIRouter()
 
 
-@app.post('/geolocalizar_endereco/', response_model=List[AdressSearch], tags=['geolocalizacao'])
-async def geolocalizar_endereco(search_endereco:AdressSearchParameters):
+@app.post('/geolocalizar_endereco', tags=['geolocalizacao'])
+async def geolocalizar_endereco(search_endereco:AdressSearchParameters)->List[AdressSearch]:
     '''Realiza a busca do endereço (geolocalização). Permite a integração com o GeoSampa por meio do parâmetro 'camadas_geosampa'.
     Para cada uma das camadas selecionadas, a ferramenta irá identificar o(s) objeto(s) da camada que intersectam o ponto que representa o 
     endereço, conforme retornado pela geolocalização'''
@@ -26,3 +26,11 @@ async def geolocalizar_endereco(search_endereco:AdressSearchParameters):
     data = buscar_endereco(endereco, **camadas_geosampa)
 
     return data
+
+@app.get('/geolocalizar_endereco_simples/{endereco}', tags=['geolocalizacao'])
+async def geolocalizar_endereco_simples(endereco:str)->GeoJsonEndereco:
+    '''Realiza a geolicalização do endereço apenas, sem integração com o GeoSampa.'''
+
+    endereco = buscar_endereco_simples(endereco)
+
+    return endereco
