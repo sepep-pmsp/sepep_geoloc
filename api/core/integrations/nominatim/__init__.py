646da49890f627b348959a3dad1d3e95dbff914f
h-pgy <hpougy@gmail.com>
2024-04-22 19:01:20 -0300

    nominatim reverse geocode client

--- api/core/integrations/nominatim/__init__.py
+++ api/core/integrations/nominatim/__init__.py
@@ -1,5 +1,7 @@
 from .nominatim import Nominatim
+from .nominatim_reverse_geocode import ReverseGeocoding
 from config import CITY, STATE, COUNTRY_ISO, NOMINATIM_EMAIL
 
 
 nominatim_address_search = Nominatim(CITY, STATE, COUNTRY_ISO, NOMINATIM_EMAIL)
+nominatim_reverse_search = ReverseGeocoding(NOMINATIM_EMAIL)
